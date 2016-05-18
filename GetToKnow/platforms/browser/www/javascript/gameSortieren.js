55d47049c4faf836907cbded5ac8285cb05bdb74
System Administrator <gwen.bischoff@hotmail.com>
2016-05-18 12:46:46 +0200

    sss working

--- GetToKnow/platforms/browser/www/javascript/gameSortieren.js
+++ GetToKnow/platforms/browser/www/javascript/gameSortieren.js
@@ -12,7 +12,7 @@ function neuerBegriff()
 	zufall = Math.floor((Math.random() * 8) + 1);  //Zahl zwischen 1 und 8
 	
 	if(zufall == 1){
-		document.getElementById("anzeige").innerHTML="Schuhgr&ouml;ße";
+		document.getElementById("anzeige").innerHTML="Schuhgr&ouml;&szlig;e";
 	}
 	if(zufall == 2){
 		document.getElementById("anzeige").innerHTML="Alter";
@@ -27,7 +27,7 @@ function neuerBegriff()
 		document.getElementById("anzeige").innerHTML="Haarl&auml;nge";
 	}
 	if(zufall == 6){
-		document.getElementById("anzeige").innerHTML="K&ouml;rpergr&ouml;ße";
+		document.getElementById("anzeige").innerHTML="K&ouml;rpergr&ouml;&szlig;e";
 	}
 	if(zufall == 7){
 		document.getElementById("anzeige").innerHTML="Farbe des Oberteils";
