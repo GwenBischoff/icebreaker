0447c9f8ca0b05a2937502f3f70e55ea7ce3b53d
System Administrator <gwen.bischoff@hotmail.com>
2016-05-21 19:41:09 +0200

    worked on wahrheit.js

--- GetToKnow/www/js/wahrheit.js
+++ GetToKnow/www/js/wahrheit.js
@@ -1,28 +1,27 @@
-var stopDeg;
-var stopRounds;
-var elem;
-var roundsCount = 0;
-var degreesCount = 0;
+var stopDeg, stopRounds, roundsCount;
 var stop;
 var loop;
+var degreesCount = 0;
 function run(){
     stopDeg = Math.floor((Math.random() * 360) + 1);
     stopRounds = Math.floor((Math.random() * 4) + 2);
     stop = false;
-
-    while(!stop){
-        setTimeout(rotateArrow, 100);  
-        if (degreesCount == stopDeg && roundsCount == stopRounds){
-            stop =true;
-        } 
-    };
+    roundsCount = 0;
+    degreesCount = 0;
+    rotateArrow();
 }
 function rotateArrow(){
-    degreesCount++;
-    if(degreesCount > 359){
-        degreesCount = 1;
-        roundsCount++;
-    }
-    document.getElementById("arrow").style.transform = "rotate("+degreesCount+"deg)";
+    while(!stop){ 
+        document.getElementById("arrow").style.transform = "rotate("+degreesCount+"deg)";
+        loop = setTimeout('rotateArrow()', 10);
+        degreesCount++;
+        if(degreesCount > 359){
+            degreesCount = 1;
+            roundsCount++;
+        }
+        if (degreesCount == stopDeg && roundsCount == stopRounds){
+            stop =true;
+        }
+    } 
 }
 
