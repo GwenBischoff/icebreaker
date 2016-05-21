5e596c78a11655eb8fa3518d12f245c89533a237
System Administrator <gwen.bischoff@hotmail.com>
2016-05-21 20:49:38 +0200

    wahrheit.js can only started once while running

--- GetToKnow/platforms/browser/www/js/wahrheit.js
+++ GetToKnow/platforms/browser/www/js/wahrheit.js
@@ -1,25 +1,44 @@
 var stopDeg, stopRounds, roundsCount;
-var stop;
-var loop;
+var stop, loop; 
+var start = true;
 var degreesCount = 0;
 function run(){
-    stopDeg = Math.floor((Math.random() * 360) + 1);
-    stopRounds = Math.floor((Math.random() * 4)+2);
-    stop = false;
-    roundsCount = 0;
-    rotateArrow();
+    if (start){
+        /*Creats random degree and amount of rounds*/
+        stopDeg = Math.floor((Math.random() * 360) + 1);
+        stopRounds = Math.floor((Math.random() * 4)+2);
+        
+        /*Boolean to stop when random created numbers are reached*/
+        stop = false;
+
+        /*Sets rounds to zero when new round started*/
+        roundsCount = 0;
+
+        rotateArrow();
+        
+        /*Prevents from starting again before action is performed*/
+        start = false;
+    }
 }
 function rotateArrow(){
-    if(!stop){ 
+    if(!stop){
+        /*Roatate image*/ 
         document.getElementById("arrow").style.transform = "rotate("+degreesCount+"deg)";
+        
+        /*Self calling funtion*/
         loop = setTimeout('rotateArrow()', 0.1);
+        
+        /*Increases degrees & rounds*/
         degreesCount++;
         if(degreesCount > 359){
             degreesCount = 1;
             roundsCount++;
-        }    
+        } 
+
+        /*Stop numbers reached?*/
         if(roundsCount >= stopRounds && degreesCount >= stopDeg){
-            stop =true;    
+            stop =true;  
+            start = true;  
         }
     } 
 }
