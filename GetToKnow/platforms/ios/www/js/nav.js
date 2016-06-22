a5560be94ba57e92ab2941f2bff6132d578a9e26
Gwen <gwen.bischoff@hotmail.com>
2016-06-22 22:07:41 +0200

    Bugfix Menü, Layout von Besprechung auf alle Spiele angewendet

--- GetToKnow/platforms/ios/www/js/nav.js
+++ GetToKnow/platforms/ios/www/js/nav.js
@@ -1,17 +1,22 @@
+$( document ).ready(function() {
+
 	/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-	function openNav() {
+	$(".sideNavIcon").click(function() {
 	    var sidenav = document.getElementsByClassName("sideNav");
 	    Array.prototype.forEach.call(sidenav, function(el) {
+	    	console.log("open");
 	    	el.style.width = "20em";
-	    	$('.gameBtns').button('disable'); 
+	    	
 		});
-	}
+	});
 
 	/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-	function closeNav() {
+	$(".closeBtn").click(function() {
 	    var sidenav = document.getElementsByClassName("sideNav");
 	    Array.prototype.forEach.call(sidenav, function(el) {
+	    	console.log("close");
 	    	el.style.width = "0";
-	    	$('.gameBtns').button('enable');
+	    	
 		});
-	}
+	});
+});
